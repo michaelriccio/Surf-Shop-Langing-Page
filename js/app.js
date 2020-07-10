d31a7f0af34f2367a85801d4f920ebfb97fe8900
michaelriccio <fchs219376@gmail.com>
2020-07-10 12:01:19 -0400

    Moving javascript variables

--- js/app.js
+++ js/app.js
@@ -13,30 +13,53 @@ const g = document.querySelector('.g');
 const c = document.querySelector('.c');
 const topList = document.querySelectorAll('li.top-list');
 
-let scrollSearch = document.querySelector('.search').scrollY;
-let scrollBoard = document.querySelector('.board').scrollY;
-let scrollMedia = document.querySelector('.media').scrollY;
-let scrollGoals = document.querySelector('.goals').scrollY;
-let scrollCustomer = document.querySelector('.customer').scrollY;
+let search = document.querySelector('.search');
+let board = document.querySelector('.board');
+let media = document.querySelector('.media');
+let goals = document.querySelector('.goals');
+let customer = document.querySelector('.customer');
+
 
 // 1. Make location change with scroll position.
 
 
+//watching you scroll
+window.addEventListener('scroll',currentLocation);
 
-let currentScroll = scrollY/document.documentElement.scrollHeight;
+//sensing the distance from the top of body, finding the height of the whole doc, updating bar width
+function currentLocation() {
+    let currentScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let percentScrolled = (currentScroll / height) * 100;
+    let progressBar = document.querySelector('#progress-bar');
+    progressBar.style.cssText = 'width:' + percentScrolled + '%;';
 
+    //creating scroll positions for each main.article
+    let currentSeach = search.scrollTop;
+    let currentBoard = board.scrollTop;
+    let currentMedia = media.scrollTop;
+    let currentGoals = goals.scrollTop;
+    let currentCustomer = customer.scrollTop;
 
-window.addEventListener('scroll', function(){
+    //comparing them to the currentScroll
+    if (currentSeach >= currentScroll > currentBoard) {locationList.style.cssText = 'transform: translateY(0);';}
+    else if (currentBoard >= currentScroll > currentMedia) {locationList.style.cssText = 'transform: translateY(2em);';}
+    else if (currentMedia >= currentScroll > currentGoals) {locationList.style.cssText = 'transform: translateY(4em);';}
+    else if (currentGoals >= currentScroll > currentCustomer) {locationList.style.cssText = 'transform: translateY(6em);';}
+    else if (currentCustomer >= currentScroll) {locationList.style.cssText = 'transform: translateY(8em);';}
     
-});
-
+    //*you need to figure out why this comes back and undefined even when defined globally
+    //*distance = window.pageYOffset + scrollBoard.getBoundingClientRect().top;
+}
+//*let distance;
 
 // 2. Allow Navigation with scroll locationList.
-const selecting = (item, index) => {
-    topList[index].addEventListener('click', function(){console.log("hi");})};
-// };
 
-// topList.forEach(selecting);
+const selecting = (item, index) => {
+    topList[index].addEventListener('click', function(){console.log("hi");})
+};
+
+topList.forEach(selecting);
 
 
 
@@ -57,7 +80,7 @@ const boardDeals = document.querySelector('.board-deals');
 
 // 4. Highlight current board tab.
 
-let tab = document.querySelectorAll('.tab');
+const tab = document.querySelectorAll('.tab');
 
 // Get rid of all pink backgrounds, then add it to the clicked one.
 for(let i=0; i < tab.length; i++) {
@@ -68,4 +91,9 @@ function active(ev){
     tab.className.remove('bg-active');
     ev.target.classList.add('bg-active');
 }
+
+
+
+
+
 // 5. MAYBE Carousel for surfing videos.
